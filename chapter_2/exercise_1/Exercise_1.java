4fa89f5a15f8203ec3c407648f5b4dd58e797bde
Bartlomiej Laskowski <frameture@gmail.com>
2016-09-21 21:04:07 +0200

    Update Exercise_1.java

--- chapter_2/exercise_1/Exercise_1.java
+++ chapter_2/exercise_1/Exercise_1.java
@@ -1,16 +1,18 @@
 public class Exercise_1 {
 	public static void main(String[] args) {
+		// introducing to the main() subroutine;
+		// working with standard output destination and Strings;
 		System.out.println();
 		System.out.println("   **********         ***");
 		System.out.println("   **      ***        ***");
 		System.out.println("   **       **        ***");
-	  	System.out.println("   **     **          ***");
-    	System.out.println("   **    **           ***");
-    	System.out.println("   ******             ***");
-    	System.out.println("   **    **           ***");
-    	System.out.println("   **      **         ***");	    
-	    System.out.println("   **       **        ***");	    
-	  	System.out.println("   **      ***        *************");   
+		System.out.println("   **     **          ***");
+		System.out.println("   **    **           ***");
+		System.out.println("   ******             ***");
+		System.out.println("   **    **           ***");
+		System.out.println("   **      **         ***");
+		System.out.println("   **       **        ***");
+		System.out.println("   **      ***        *************");
 		System.out.println("   **********    **   *************");	    
 	}
 }
