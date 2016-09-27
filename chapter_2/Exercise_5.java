677fafcc43ba1c4c5dc90d66ce584c6bae081a9d
Bartlomiej Laskowski <frameture@gmail.com>
2016-09-27 20:54:45 +0200

    Create Exercise_5.java

---
+++ chapter_2/Exercise_5.java
@@ -0,0 +1,19 @@
+/** If you have N eggs, then you have N/12 dozen eggs, with N%12 eggs left 
+ * over. (This is (solution)essentially the definition of the / and % operators
+ * for integers.) Write a program that asks the user how many eggs she has and 
+ * then tells the user how many dozen eggs she has and how many extra eggs are
+ * left over. A gross of eggs is equal to 144 eggs. Extend your program so that
+ * it will tell the user how many gross, how many dozen, and how many left over
+ * eggs she has. For example, if the user says that she has 1342 eggs, then your
+ * program would respond with:
+ * 
+ * Your number of eggs is 9 gross, 3 dozen, and 10 since 1342 is equal 
+ * to 9*144 + 3*12 + 10.
+ */
+public class Exercise_5 {
+
+	public static void main(String[] args) {
+		
+		TextIO.getlnInt();
+	}
+}
