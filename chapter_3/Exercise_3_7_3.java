60b56a889e7913c41e53bfb71281c0e8be7ce68d
Bartlomiej Laskowski <frameture@gmail.com>
2016-10-05 22:52:56 +0200

    Create Exercise_3_7_3.java

---
+++ chapter_3/Exercise_3_7_3.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * How many different people do you have to check before you've found at least 
+ * one person with a birthday on each of the 365 days of the year?
+ *
+ */
+
+public class Exercise_3_7_3 {
+	public static void main(String[] args) {
+		// booleans of occurred dates;
+		boolean[] dates = new boolean[365];	
+		int peopleCounter = 0, 
+				birthdaypeopleCounter = 0;
+		
+		// looping until 365 different birthdays are found;
+		int date;
+		while (birthdaypeopleCounter < 365) {
+			date = (int)(365 * Math.random());
+			peopleCounter++;
+			if (dates[date] == false ) {
+				birthdaypeopleCounter++;
+				dates[date] = true;
+				}
+		}
+		System.out.println("There were " + (birthdaypeopleCounter) + " people "
+				+ "with different birthday dates and it took " + peopleCounter +
+				" random people to find them.");	
+	}
+}
