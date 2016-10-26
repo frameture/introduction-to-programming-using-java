6ed37fdb47fb1a1e7acc75c5c6279ea09925a6cd
Bartlomiej Laskowski <frameture@gmail.com>
2016-10-26 13:50:53 +0200

    Create AdditionQuestion.java

---
+++ chapter_5/exercise_5_6/AdditionQuestion.java
@@ -0,0 +1,13 @@
+public class AdditionQuestion {
+	    public int a, b;  // The numbers in the problem.
+	    public AdditionQuestion() { // constructor
+	        a = (int)(Math.random() * 50 + 1);
+	        b = (int)(Math.random() * 50);
+	    }
+	    public String getQuestion() {
+	        return "What is " + a + " + " + b + " ?";
+	    }
+	    public int getCorrectAnswer() {
+	        return a + b;
+	    }
+} 
