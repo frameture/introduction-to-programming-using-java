a4ed12e8d206e2652b8adf96cc56c6d5639578c8
Bartlomiej Laskowski <frameture@gmail.com>
2016-11-25 10:45:05 +0100

    Change indentation

--- chapter_3/Exercise_3_7_1.java
+++ chapter_3/Exercise_3_7_1.java
@@ -7,19 +7,19 @@
  */
 
 public class Exercise_3_7_1 {
-	public static void main(String[] args) {
-		int counter = 0;
-		int date1, date2, date3;
-		while(true){
-			date1 = (int)(365 * Math.random() + 1);
-			date2 = (int)(365 * Math.random() + 1);
-			date3 = (int)(365 * Math.random() + 1);
-			counter++;
-			if(date1 == date2 && date2== date3)
-				break;
-			}
-		counter *= 3;
-		System.out.println("There were needed "+ counter +" random people to find "
-				+ "three with same birthday date.");
-	}
+  public static void main(String[] args) {
+    int counter = 0;
+    int date1, date2, date3;
+    while(true) {
+      date1 = (int)(365 * Math.random() + 1);
+      date2 = (int)(365 * Math.random() + 1);
+      date3 = (int)(365 * Math.random() + 1);
+      counter++;
+      if (date1 == date2 && date2== date3)
+        break;
+      }
+      counter *= 3;
+      System.out.println("There were needed "+ counter +" random people to find "
+			 + "three with same birthday date.");
+  }
 }
