af3a180e4d0f4bcbd043009a72681ced1d09621f
Bartlomiej Laskowski <frameture@gmail.com>
2016-12-10 21:49:54 +0100

    Exercise_9_2 /upload

---
+++ chapter_9/Exercise_9_2.java
@@ -0,0 +1,24 @@
+// depends on TextIO Class
+
+/**
+ * Exercise 7.7 asked you to read a file, make an alphabetical list of all the
+ * words that occur in the file, and write the list to another file. In that
+ * exercise, you were asked to use an ArrayList<String> to store the words.
+ * Write a new version of the same program that stores the words in a binary
+ * sort tree instead of in an arraylist.
+ *
+ */
+public class Exercise_9_2 {
+
+
+
+
+
+
+
+
+
+      
+
+
+}
